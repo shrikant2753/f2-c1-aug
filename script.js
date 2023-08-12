e8b98c29bf6beb83528264626b301f8a4256dcae
shrikant2753 <shrikantrp.00@gmail.com>
2023-08-12 12:06:58 +0530

    solution

--- script.js
+++ script.js
@@ -9,18 +9,33 @@ let arr = [
 
 function PrintDeveloper() {
   //Write your code here , just console.log
+  const developers = arr.filter(employee =>employee.profession==='developer');
+  console.log(developers);
 }
 
 function addData() {
   //Write your code here, just console.log
+  const newEmployee = { id: 4, name: 'susan', age: 20, profession: 'intern' };
+  arr.push(newEmployee);
+  console.log('Array after adding data:', arr);
+
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+  const updatedArr = arr.filter(employee=>employee.profession !=='admin')
+  console.log(updatedArr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  const additionalEmployees = [
+    { id: 5, name: 'michael', age: 22, profession: 'developer' },
+    { id: 6, name: 'emily', age: 21, profession: 'developer' }
+  ];
+  
+  const combinedArray = arr.concat(additionalEmployees);
+  console.log('Concatenated Array:', combinedArray);
 }
 
 // Here is an example of how functions work,
